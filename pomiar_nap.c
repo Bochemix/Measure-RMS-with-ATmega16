08f8b716c2660e67e97779963651c660839dea72
Bochemix <61210768+Bochemix@users.noreply.github.com>
2020-02-20 18:26:23 +0100

    Create pomiar_nap.c

---
+++ pomiar_nap.c
@@ -0,0 +1,23 @@
+/*
+ * pomiar_nap.c
+ *
+ *  Created on: Apr 9, 2015
+ *      Author: juju
+ */
+#include "pomiar_nap.h"
+
+		uint16_t pomiar(uint8_t channel)		// function for measure voltage
+		{
+			ADMUX = (ADMUX & 0xF8) | channel;
+			ADCSRA |= (1<<ADSC);
+			while(ADCSRA & (1<<ADSC));
+			printf ("%ud  \n\r",TCNT1);
+			return ADCH;
+		}
+		void init_adc()
+		{
+			ADMUX	|=	(1<<REFS0);
+			ADCSRA	|=	(1<<ADEN)|(1<<ADPS1)|(1<<ADPS0);
+		}
+
+
