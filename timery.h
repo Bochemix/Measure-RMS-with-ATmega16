28462185472e139a3adc7624d615d5bc6305c1dd
Bochemix <61210768+Bochemix@users.noreply.github.com>
2020-02-20 18:24:36 +0100

    Create timery.h

---
+++ timery.h
@@ -0,0 +1,17 @@
+/*
+ * timery.h
+ *
+ *  Created on: Apr 9, 2015
+ *      Author: juju
+ */
+
+#ifndef TIMERY_H_
+#define TIMERY_H_
+
+volatile uint16_t licznik;
+volatile uint8_t flag;
+volatile uint8_t czas;
+
+void init_timera();
+
+#endif /* TIMERY_H_ */
